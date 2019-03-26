56aba426575b023b3500b72ba2add2ba4f332bb6
aliciea <alicieadawnr@gmail.com>
2019-03-26 15:52:32 -0400

    weekendreview.js

--- weekendreview.js
+++ weekendreview.js
@@ -22,72 +22,93 @@ prompt("what is your user name");
 
 //task 8: Print intergers multiples of 3 as long as integers are less than 35
 
-let i = 1;
-while(i < 35){
-	console.log(i * 3); 
-	i++;
-} 
+// let i = 1;
+// while(i < 35){
+// 	console.log(i * 3); 
+// 	i++;
+// } 
 
-//Task 11: Pirnt intergers mulitples of 5 as long as it's less than 100
+// //Task 11: Pirnt intergers mulitples of 5 as long as it's less than 100
 
-let i = 1;
-while(i < 100){
-	console.log(i * 5); 
-	i++;
-} 
+// let i = 1;
+// while(i < 100){
+// 	console.log(i * 5); 
+// 	i++;
+// } 
 
-//Task 13: Using a while loop print integers btwn 0 and 20. 
-All numbers divisible by 2 should be multiplied by 3 before they are output. 
-All other integers should not be output. 
-Need help with this problem...
-let i = 20;
-while(i % 2 === 0 && i * 3){
-	console.log(i);
-	i++;
-}
+// //Task 13: Using a while loop print integers btwn 0 and 20. 
+// All numbers divisible by 2 should be multiplied by 3 before they are output. 
+// All other integers should not be output. 
+// Need help with this problem...
+// let i = 20;
+// while(i % 2 === 0 && i * 3){
+// 	console.log(i);
+// 	i++;
+// }
 
-//Task 14: Using a while loop, print out all prime numbers between 0 - 20
+// //Task 14: Using a while loop, print out all prime numbers between 0 - 20
 
-let i = 0;
-while(i < 20){
-	console.log(i);
-	i+=2;
-}
+// let i = 0;
+// while(i < 20){
+// 	console.log(i);
+// 	i+=2;
+// }
 
-//Task 15: Nando went to the vending machine to buy himself a cookie. 
-The cookie costs $4 dollars. He paid with a $10 bill, 
-the vending machine paid him back in quarters. 
-Write a loop that says how many quarters he got in return.
+// //Task 15: Nando went to the vending machine to buy himself a cookie. 
+// The cookie costs $4 dollars. He paid with a $10 bill, 
+// the vending machine paid him back in quarters. 
+// Write a loop that says how many quarters he got in return.
 
 let i = 4;
+
 while(i < 10){ 
 if(i % 24 === 0){
 	console.log(i);
 	i--;
-}
+};
 
-//Task 24
-es5 
-function addition(a, b, c){
-	var z
-	z =  a + b + c;
-	return z;
-}
-es6
-let division = (a, b, c) => a / b / c;
-console.log(division(3, 6, 9 / 3));
-vs task 25/26 of how es5 & es6 version should be written for task 24... 
+// let quarters = 0;
+// let value = 10.00 - 4.00;
+// while(value >= 0.25){
+// 	quarters++;
+// 	value-=0.25;
+// }
+// console.log(quarters);
 
-//task 32
-function let(){
-	let (x,y);
-	console.log(1 + 1 / 20);
-}
+// //Task 24
+// es5 
+// function addition(a, b, c){
+// 	var z
+// 	z =  a + b + c;
+// 	return z;
+// }
+// es6
+// let division = (a, b, c) => a / b / c;
+// console.log(division(3, 6, 9 / 3));
+// vs task 25/26 of how es5 & es6 version should be written for task 24... 
+
+// //task 32
+// function let(){
+// 	let (x,y);
+// 	console.log(1 + 1 / 20);
+// }
  
 //task 33 logical question
 //need help with questions 33/34
 
+
 //task 34 fizz buzz
+// for(var i = 1; i < 100; i++){	
+// 	if(i % 3 == 0 && i % 5 == 0){
+// 	  console.log("fizzbuzz");
+// 	}else if(i % 3 == 0){ 
+// 	  console.log("fizz");
+// 	}else if(i % 5 == 0){
+// 	  console.log("buzz");
+// 	}else{
+// 	  console.log(i);
+// 	} 
+// }
 
 
 
