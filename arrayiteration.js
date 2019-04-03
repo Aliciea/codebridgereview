3593e2633f3b1e06bb2b5b45f38290c5a2733dce
aliciea <alicieadawnr@gmail.com>
2019-04-03 10:05:54 -0400

    arrayiteration.js

---
+++ arrayiteration.js
@@ -0,0 +1,69 @@
+//forLoop
+//loop over an array using a for loop,
+//we need to make use of the array's length property
+
+// var colors = ["red", "orange", "yellow", "green"];
+
+// for(var i = 0; i < colors.length; i++) {
+// 	console.log(colors[i]);
+// }
+//##################################################################
+
+//forEach is a method
+//.forEach takes a callback function, 
+//that callback function is expected to have at least 1, 
+//but up to 3, arguments. 
+
+var array1 = ['a', 'b', 'c'];
+
+array1.forEach(function(element) {
+  console.log(element);
+});
+
+//js provides an easy built-in way to iterating over an array
+// arr.forEach(someFunction)
+
+// var colors = ["red", "orange", "green"];
+
+// colors.forEach(function(color) {
+// //color is a placeholder, call it whatever you want
+// console.log(color);	
+// });
+
+// //define a function and past another function in the forEach
+
+// var colors = ["red", "orange", "yellow", "green"];
+// //name the function inside the forEach function anything you want i.e. iLoveDogs
+// colors.forEach(function(iLoveDogs) {
+// 	console.log("INSIDE THE FOREACH" + iLoveDogs);
+// });
+
+// //exercise 
+// //this is a tricky exercise that is meant confused you 
+
+// var numbers = [1,2,3,4,5,6,7,8,9,10];
+// var colors = ["red", "orange", "yellow", "green"];
+
+// numbers.forEach(function(color) { //
+// //we are running the numbers.forEach in the function
+// 	if(color% 3 === 0) {
+// 		console.log(color);
+// 	}
+// });
+
+//rewrite above code and translate it into a forLoop
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var colors = ["red", "orange", "yellow", "green"];
+for(i = 0; i < numbers.le
+	ngth; i++){
+	if(numbers% 3 === 0){
+		console.log(numbers);
+	}
+}
+
+
+
+
+
+
--- todolist.js
+++ todolist.js
@@ -7,7 +7,10 @@ var input = prompt("What would you like to do?");
 while(input !== "quit") {
 	//handle input
 	if(input === "list") {
-	console.log(todos);
+		todos.forEach(function(todo){
+			console.log(todo);
+		});
+	// console.log(todos);
 	} else if(input === "new") { 
 	//ask for new todo
 	var newTodo = prompt("Enter new todo");
