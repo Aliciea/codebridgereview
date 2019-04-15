1d5dae1c0960e0395619bac87c3bc1c72394d347
aliciea <alicieadawnr@gmail.com>
2019-04-15 15:35:35 -0400

    task3.3/fizzbuzz.html

---
+++ task3.3/gradeCalculatorFunc.js
@@ -0,0 +1,39 @@
+//create a calculator with an if else statement and a switch statement
+
+//if else statement
+
+var gradeCalulator = grade;
+
+if(gradeCalulator >= 90) {
+	console.log('A');
+}else if(gradeCalulator >= 80) {
+	console.log('B');
+}else if(gradeCalulator >= 70) {
+	console.log('C');
+}else if(gradeCalulator >=55) {
+	console.log('D');
+}else{
+	console.log('F');
+} 
+
+//switch statement
+
+var gradeCalulator = grade;
+
+swith(gradeCalulator) {
+	case '90';
+	console.log('A');
+	break;
+	case '80';
+	console.log('B');
+	break;
+	case '70';
+	console.log('C');
+	break;
+	case '55';
+	console.log('D');
+	break;
+	case '50';
+	console.log('F');
+	break;
+}
